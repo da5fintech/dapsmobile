509dc12edbe2b2ae3364115184df90764b0d28c5
jpeeicruz <jp.delacruz@kabootekph.com>
2021-01-27 16:35:12 +0800

    Sidebar menu layout added #16

--- lib/screens/services-screen.dart
+++ lib/screens/services-screen.dart
@@ -7,6 +7,7 @@ import 'package:swipe/common/constants.dart';
 import 'package:swipe/common/size.config.dart';
 
 import 'package:swipe/common/widgets/amount-widget.dart';
+import 'package:swipe/common/widgets/drawer-menu-widget.dart';
 import 'package:swipe/store/application-store.dart';
 import 'package:swipe/common/widgets/main-app-bar.widget.dart';
 import 'package:swipe/common/widgets/service-button.dart';
@@ -34,20 +35,7 @@ class _ServicesScreenState extends State<ServicesScreen> {
     return Scaffold(
         // backgroundColor: Constants.backgroundColor2,
         key: _drawerKey,
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              ListTile(
-                title: Text("Logout"),
-                trailing: Icon(Icons.arrow_forward),
-                onTap: () async {
-                  await store.authService.logout();
-                  Get.toNamed("/login");
-                },
-              ),
-            ],
-          ),
-        ),
+        drawer: DrawerMenuWidget(),
         appBar: MainAppBarWidget(
           elevation: 0,
           onPressed: () {
