5c7f026a41c741db42c64f4540ee96407a998ba7
jpeeicruz <jp.delacruz@kabootekph.com>
2021-02-17 10:16:46 +0800

    generate QR ui & custom QR #150 #151

---
+++ lib/screens/direct-send/direct-request-form-screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:swipe/common/constants.dart';
+import 'package:swipe/common/size.config.dart';
+import 'package:swipe/common/widgets/sub-app-bar.widget.dart';
+
+class DirectRequestFormScreen extends StatefulWidget {
+  final Function onSave;
+
+  DirectRequestFormScreen({this.onSave});
+
+  @override
+  _DirectRequestFormScreenState createState() =>
+      _DirectRequestFormScreenState();
+
+
+}
+
+class _DirectRequestFormScreenState extends State<DirectRequestFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    ThemeData td = createThemePurpleOnWhite(context);
+    return Theme(
+      data: td,
+      child: Scaffold(
+        appBar: SubAppbarWidget(
+          title: "My Custom QR",
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ListView(
+                  children: [
+                    SizedBox(height: 20),
+                    TextFormField(
+                      autofocus: true,
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (val) {
+                      },
+                      onSaved: (v) {
+                      },
+                      decoration: InputDecoration(
+                        errorStyle: TextStyle(
+                            color: COLOR_GRAY, fontSize: 12, height: 0.3),
+                        labelText: 'Nickname (Optional)',
+                        floatingLabelBehavior:
+                        FloatingLabelBehavior.always,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      autofocus: true,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.phone,
+                      onFieldSubmitted: (val) {
+                      },
+                      onSaved: (v) {
+                      },
+                      validator: (text) {
+                        if(text.isEmpty) {
+                          return "Amount is Required!";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        errorStyle: TextStyle(
+                            color: COLOR_DANGER, fontSize: 12),
+                        labelText: DIRECT_SEND_FORM_SCREEN_AMOUNT,
+                        floatingLabelBehavior:
+                        FloatingLabelBehavior.always,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 25, right: 25),
+              child: SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  // shape: ,
+                  onPressed: () => widget.onSave(),
+                  child: Text(
+                    'Save',
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+}
--- lib/screens/direct-send/direct-send-screen.dart
+++ lib/screens/direct-send/direct-send-screen.dart
@@ -1,34 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:swipe/common/constants.dart';
 import 'package:swipe/common/size.config.dart';
 import 'package:swipe/common/widgets/sub-app-bar.widget.dart';
+import 'package:swipe/screens/direct-send/direct-request-form-screen.dart';
 
 class DirectSendScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    ThemeData td = createThemePurpleOnWhite(context);
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return DefaultTabController(
       length: 2,
-      child: Theme(
-        data: td,
-        child: Scaffold(
-          appBar: SubAppbarWidget(
-            height: 90.0,
-            title: DIRECT_SEND_SCREEN_TITLE_TEXT,
-            bottom: TabBar(
-              indicatorColor: COLOR_ORANGE,
-              tabs: [
-                Tab(icon: Text('Send')),
-                Tab(icon: Text('Request')),
-              ],
-            ),
+      child: Scaffold(
+        appBar: SubAppbarWidget(
+          height: 90.0,
+          title: DIRECT_SEND_SCREEN_TITLE_TEXT,
+          bottom: TabBar(
+            indicatorColor: COLOR_ORANGE,
+            tabs: [
+              Tab(icon: Text('Send')),
+              Tab(icon: Text('Request')),
+            ],
           ),
-          body: TabBarView(
-            children: [
-              ListView(
+        ),
+        body: TabBarView(
+          children: [
+            //Send
+            Container(
+              color: Colors.white,
+              child: ListView(
                 children: [
                   Container(
                     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
@@ -50,7 +55,8 @@ class DirectSendScreen extends StatelessWidget {
                     ),
                     child: ListTile(
                       onTap: () {
-                        Get.toNamed('/services/direct-send/direct-send-form-screen');
+                        Get.toNamed(
+                            '/services/direct-send/direct-send-form-screen');
                       },
                       title: Text(
                         DIRECT_SEND_SCREEN_DIRECT_SEND_TEXT,
@@ -131,7 +137,8 @@ class DirectSendScreen extends StatelessWidget {
                     ),
                     child: ListTile(
                       onTap: () {
-                        Get.toNamed('/services/remittance/remittance-categories-screen');
+                        Get.toNamed(
+                            '/services/remittance/remittance-categories-screen');
                       },
                       title: Text(
                         DIRECT_SEND_SCREEN_REMITTANCE_CENTERS_TEXT,
@@ -146,9 +153,134 @@ class DirectSendScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              Container(),
-            ],
-          ),
+            ),
+            //Request
+            Container(
+              height: height,
+              width: width,
+              padding: EdgeInsets.only(top: 20, right: 10, left: 10),
+              child: Column(
+                children: [
+                  Text(
+                    APP_NAME,
+                    style: GoogleFonts.roboto(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      letterSpacing: 15,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: height * 0.45,
+                    width: width * 0.90,
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: QrImage(
+                              data: "1232456",
+                              size: height * 0.25,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: (){
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (_) => DirectRequestFormScreen())
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Jose Paulo',
+                                  style: GoogleFonts.roboto(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16,
+                                    color: COLOR_DARK_PURPLE,
+                                  ),
+                                ),
+                                Icon(Icons.edit, color: COLOR_DARK_PURPLE),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 40,
+                            color: COLOR_ORANGE,
+                            child: Center(
+                              child: InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (_) => DirectRequestFormScreen())
+                                  );
+                                },
+                                child: Text(
+                                  '+ ADD AMOUNT',
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.roboto(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14,
+                                    height: 1.5,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 32, right: 10, left: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Icon(Icons.download_sharp, color: Colors.white),
+                              Text(
+                                'Download',
+                                style: GoogleFonts.roboto(
+                                  color: Colors.white,
+                                  height: 1.5,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Icon(Icons.share, color: Colors.white),
+                              Text(
+                                'Share',
+                                style: GoogleFonts.roboto(
+                                  color: Colors.white,
+                                  height: 1.5,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
