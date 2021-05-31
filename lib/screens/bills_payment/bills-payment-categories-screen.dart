c7e29dacffed5dfd3b910f3fce7280539d4b3afa
jpeeicruz <jp.delacruz@kabootekph.com>
2021-05-31 14:37:47 +0800

    fix: bills payment categories screen crash when using android hardware backbutton during api calls #423

--- lib/screens/bills_payment/bills-payment-categories-screen.dart
+++ lib/screens/bills_payment/bills-payment-categories-screen.dart
@@ -8,6 +8,7 @@ import 'package:overlay_screen/overlay_screen.dart';
 import 'package:swipe/common/common-alert.dialog.dart';
 import 'package:swipe/common/constants.dart';
 import 'package:swipe/common/size.config.dart';
+import 'package:swipe/common/util.dart';
 import 'package:swipe/common/widgets/sub-app-bar.widget.dart';
 import 'package:swipe/models/product-model.dart';
 import 'package:swipe/store/application-store.dart';
@@ -278,14 +279,16 @@ class _BillsPaymentCategoriesScreenState
 
   _handleButtonClick(String category) async {
     if (store.selectedBillerCategory != category) {
-      OverlayScreen().show(context);
+      modalHudLoad(context);
+      // OverlayScreen().show(context);
       store.selectedBillerCategory = category;
       print(category);
       var billers =
           await store.billsPaymentService.getBillersByCategory(category);
       store.billers = billers;
 
-      OverlayScreen().pop();
+      // OverlayScreen().pop();
+      Navigator.pop(context);
     }
 
     Get.toNamed('/services/bills-payment/bills-payment-billers-screen');
