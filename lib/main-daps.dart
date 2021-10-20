88b2daa7be8ef5935dcd38e3b2aa50aa51cfdc71
jpeeicruz <jp.delacruz@kabootekph.com>
2021-10-20 16:10:16 +0800

    Create a new Flavor/package name for dapsmobile #8

---
+++ lib/main-daps.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flavor/flavor.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+Future main() async {
+  Flavor.create(
+    Environment.dev,
+    color: Colors.green,
+    name: 'PREVIEW',
+    properties: {
+      namespaceKey: "",
+      packageIdKey: "ph.kabootek.dapsmobile",
+      dapsUrl: "https://accounts.daps.ph/staging/index.php",
+    },
+  );
+  setupApp();
+}
