7abb7026d5e62124bec381ed02751daf9af30e82
jpeeicruz <jp.delacruz@kabootekph.com>
2021-11-15 13:43:46 +0800

    update terms and condition from swipe into daps #34

--- lib/screens/markdowns-views/terms-condition-screen.dart
+++ lib/screens/markdowns-views/terms-condition-screen.dart
@@ -29,7 +29,7 @@ const privacy = """
 
 **Background**
 
-This privacy policy sets out the commitment of DA5 and KabootekPH to collect and process personal information, how we may use this information, and other important topics relating to your privacy and data protection in accordance with applicable laws and regulations relating to data privacy such as the Philippine Data Privacy Act of 2012 and its IRR. This company respects and values your data privacy rights, and makes sure that all personal data collected from you are processed in adherence to the general principles of transparency and legitimate purpose. It also applies to personal informationDA5 and KabootekPH may otherwise collect though our  **SWIPE APP**, through our products   and   services,   and   business   Partners.Information   that   we   receive   from customers, whether constituting personal data, are generally protected as privileged communications, and covered by our responsibility to our customers to keep that information confidential.
+This privacy policy sets out the commitment of DA5 and KabootekPH to collect and process personal information, how we may use this information, and other important topics relating to your privacy and data protection in accordance with applicable laws and regulations relating to data privacy such as the Philippine Data Privacy Act of 2012 and its IRR. This company respects and values your data privacy rights, and makes sure that all personal data collected from you are processed in adherence to the general principles of transparency and legitimate purpose. It also applies to personal informationDA5 and KabootekPH may otherwise collect though our  **DAPS APP**, through our products   and   services,   and   business   Partners.Information   that   we   receive   from customers, whether constituting personal data, are generally protected as privileged communications, and covered by our responsibility to our customers to keep that information confidential.
 
 **Definitions**
 
@@ -47,7 +47,7 @@ This privacy policy sets out the commitment of DA5 and KabootekPH to collect and
 
 **Processing of Personal Data** 
 
-This company collects the basic contact information of clients and customers, including their full name, address, email address, contact number, together with the products that they would like to avail in the SWIPE APP. 
+This company collects the basic contact information of clients and customers, including their full name, address, email address, contact number, together with the products that they would like to avail in the DAPS APP. 
 
 This company will ensure that personal data under its custody are protected against any accidental or unlawful destruction, alteration and disclosure as well as against any other unlawful processing. The company will implement appropriate security measures in storing collected personal information, depending on the nature of the information. 
 
@@ -58,7 +58,7 @@ Personal data under the custody of the company shall be disclosed only pursuant 
 
 **Security Measure**
 
-This company will endeavor to ensure that personal data in our possession or under our control are protected against unauthorized access, collection, use, disclosure, copying, modification, disposal or similar risks.The designated Data Protection Officer is Mr. Alfred Tolentino, who is concurrently serving as the Monitoring Supervisor the Compliance and Training Department of DA5.The Data Protection Officer shall oversee the compliance of the SWIPE APP with the DPA, its IRR, and other related policies.
+This company will endeavor to ensure that personal data in our possession or under our control are protected against unauthorized access, collection, use, disclosure, copying, modification, disposal or similar risks.The designated Data Protection Officer is Mr. Alfred Tolentino, who is concurrently serving as the Monitoring Supervisor the Compliance and Training Department of DA5.The Data Protection Officer shall oversee the compliance of the DAPS APP with the DPA, its IRR, and other related policies.
 
 **Inquiries and Complaints** 
 
@@ -77,27 +77,27 @@ Submitting your data signifies that you have read and understood this Privacy No
 const terms = """
 **TERMS AND CONDITIONS**
 
-Welcome to the Swipe Application. This Application is provided by Direct Agent 5 and KabootekPH Inc. By registering/using to our service, you agree to the terms and conditions contained in this Terms of Service as well as additional terms and conditions which may subsequently apply to you. Please read them carefully. The following T&Cs shall govern the terms under which you may access and use the products of Swipe and the services associated with it.  
-Swipe reserves the right to modify these T&Cs at any time without any advance notice. Any changes to these T&Cs will be effective immediately upon posting on this page, with an updated effective date. 
+Welcome to the Daps Application. This Application is provided by Direct Agent 5 and KabootekPH Inc. By registering/using to our service, you agree to the terms and conditions contained in this Terms of Service as well as additional terms and conditions which may subsequently apply to you. Please read them carefully. The following T&Cs shall govern the terms under which you may access and use the products of Daps and the services associated with it.  
+Daps reserves the right to modify these T&Cs at any time without any advance notice. Any changes to these T&Cs will be effective immediately upon posting on this page, with an updated effective date. 
 
 **TERMS**
 
 Direct Agent 5, Inc. – A company duly registered with the Bangko Sentral ng Pilipinas (BSP) as a licensed remittance agent. It offers cash in and cash out services, mobile air-time top ups, remittance services, bill payments and Virtual Currency exchange (hereinafter “DA5 Services”
 Kabootekph- A company duly registered with the Bangko Sentral ng Pilipinas (BSP) acting as a partner of Direct Agent 5 in providing………..
 “Know Your Customer” or “KYC” shall refer to the process of verifying the identity of the customer as required by the BSP.
-“Mobile Personal Identification Number” or “MPIN” shall refer to the four (4)-digit security personal identification number of, and nominated for the Swipe Customer that is used for all Swipe transactions. 
+“Mobile Personal Identification Number” or “MPIN” shall refer to the four (4)-digit security personal identification number of, and nominated for the Daps Customer that is used for all Daps transactions. 
 Online Payments – Refers to the payment made by an online merchant over the internet. 
-Swipe App - the Swipe Service application running on mobile devices where the customer can avail of the Swipe Services or make use of the Swipe Wallet.
-Swipe Wallet – An e-money instrument that is used, but are not limited to, payments of goods and services, transfer funds and mobile airtime top-ups. It is not a deposit account, and not covered by the Philippine Deposit Insurance Corporation (PDIC). It does not earn interest.
+Daps App - the Daps Service application running on mobile devices where the customer can avail of the Daps Services or make use of the Daps Wallet.
+Daps Wallet – An e-money instrument that is used, but are not limited to, payments of goods and services, transfer funds and mobile airtime top-ups. It is not a deposit account, and not covered by the Philippine Deposit Insurance Corporation (PDIC). It does not earn interest.
 
-**REGISTRATION FOR A SWIPE ACCOUNT**
+**REGISTRATION FOR A DAPS ACCOUNT**
 
 You will be asked to provide information such as your complete name, address, date of birth, nationality, source of funds, Identification Card as provided below, in accordance with the requirements of Know-Your-Customer (“KYC”) requirements of the BSP. By accepting this terms and conditions, you are agreeing to provide necessary information for registration and verification of your identification details. Likewise, we may require you to provide additional details or information for further verification of your identity.
 
-**VERIFICATION FOR A SWIPE ACCOUNT**
+**VERIFICATION FOR A DAPS ACCOUNT**
 
 To verify your identity, you will be asked to submit a clear photo of your valid identification card. Make sure to take a photo of your ID showing your face and your signature. Failure to submit clear images will result in to delay in your verification. 
-The following are the identification cards Swipe app accepts:
+The following are the identification cards Daps app accepts:
 
 * Passport
 * Driver’s license
@@ -112,20 +112,20 @@ The following are the identification cards Swipe app accepts:
 * Company ID
 
 
-DA5 and KabootekPh reserve the right to terminate, suspend or limit your access to your Swipe Wallet and/or Services in the event that we are unable to obtain or verify our information. All collection and use of your personal data by us is subject to the Data Privacy Act of the Philippines.
+DA5 and KabootekPh reserve the right to terminate, suspend or limit your access to your Daps Wallet and/or Services in the event that we are unable to obtain or verify our information. All collection and use of your personal data by us is subject to the Data Privacy Act of the Philippines.
 
 **WARRANTIES AND DISCLAIMER** 
 
-DA5 and KabootekPh provide its Services using a commercially reasonable level of skill and care for the provision of efficient assistance in respect of your payment transactions. Other than as expressly set out herein or under additional terms as may be subsequently stipulated by Swipe Terms and Conditions and to the extent permitted by the law. Swipe does not make any warranty about our service which is provided “as is”. 
+DA5 and KabootekPh provide its Services using a commercially reasonable level of skill and care for the provision of efficient assistance in respect of your payment transactions. Other than as expressly set out herein or under additional terms as may be subsequently stipulated by Daps Terms and Conditions and to the extent permitted by the law. Daps does not make any warranty about our service which is provided “as is”. 
 
 **LIABILITY FOR OUR SERVICE**
 
-To the extent permitted by the Law, Swipe App shall not be responsible for lost profits, revenue, data financial loss or any indirect special, consequential, exemplary or punitive damages arising from the use of the Swipe Application. The total liability which Swipe may be held in accountable relative to any claims under this Agreement shall be limited to the relevant transaction amount paid to us using the online bills pay service. 
+To the extent permitted by the Law, Daps App shall not be responsible for lost profits, revenue, data financial loss or any indirect special, consequential, exemplary or punitive damages arising from the use of the Daps Application. The total liability which Daps may be held in accountable relative to any claims under this Agreement shall be limited to the relevant transaction amount paid to us using the online bills pay service. 
 In no case shall DA5 be liable for any loss or damage resulting from force majeure or any circumstances which is beyond the reasonable contemplation and control of DA5 and KabootekPH and totally independent of the will of the human beings, the consequences of which could not be reasonably foreseen at the time of the execution of the agreement.  
 
 **DATA PRIVACY and CONSENT**
 
-DA5 and KabootekPH shall keep all SWIPE Customer files in confidence in accordance with the Data Privacy Act of the Philippines. By providing the customer’s information for the purpose of availing a SWIPE Wallet, he/she consents to the processing of his/her customer data, as may be applicable. 
+DA5 and KabootekPH shall keep all DAPS Customer files in confidence in accordance with the Data Privacy Act of the Philippines. By providing the customer’s information for the purpose of availing a DAPS Wallet, he/she consents to the processing of his/her customer data, as may be applicable. 
 
 **VENUE OF LITIGATION**
 
@@ -138,17 +138,17 @@ Should any term or condition in this Agreement be rendered invalid, the other pr
 **CUSTOMER COMPLAINTS and INQUIRY**
 For your information about how to contact Direct Agent 5, Inc., please visit https://da5.com.ph. You may likewise coordinate with our Customer Service Support Department at BSD - TECHSUPPORT <bsd.techsupport@da5.com.ph> and BSD - TECHSUPPORT bsd.techsupport@da5.com.ph / +632 8958195
 
-**TERMS OF SERVICE USING THE SWIPE APP**
+**TERMS OF SERVICE USING THE DAPS APP**
 
-There are lots of features the Swipe app has to offer, and convenience is what it wants for its users. Here are the following features you can avail of through the Swipe app. 
+There are lots of features the Daps app has to offer, and convenience is what it wants for its users. Here are the following features you can avail of through the Daps app. 
 
 * **Send or Receive Money**
 
-If you want to send money for your loved ones, the Swipe app got you. It has lists of banks and remittance centers that you can set your transfer to. Likewise, receiving money is also one of the app’s features. Whatever transaction you want to make, make sure you got the right information to avoid the hassle. It would also be better to input your email address for your transaction receipt every time you send or receive money. 
+If you want to send money for your loved ones, the Daps app got you. It has lists of banks and remittance centers that you can set your transfer to. Likewise, receiving money is also one of the app’s features. Whatever transaction you want to make, make sure you got the right information to avoid the hassle. It would also be better to input your email address for your transaction receipt every time you send or receive money. 
 
 * **Pay Bills**
 
-Pay your utility bills through the Swipe app. You just need to select the biller, input the amount, pay, and wait for the confirmation. From different utility and internet bills, you can use the app to clear your balance and avoid disconnection. You’ll see the lists of various billers such as:
+Pay your utility bills through the Daps app. You just need to select the biller, input the amount, pay, and wait for the confirmation. From different utility and internet bills, you can use the app to clear your balance and avoid disconnection. You’ll see the lists of various billers such as:
 
 * Electric utilities
 * Water utilities
@@ -165,23 +165,23 @@ Pay your utility bills through the Swipe app. You just need to select the biller
   
 * **Buy Load**
 
-The Swipe app can reload your prepaid and internet cards anytime. From the smallest to the highest possible eLoad amount, the Swipe app got your back. Enter your mobile number, pay, and wait for confirmation. 
+The Daps app can reload your prepaid and internet cards anytime. From the smallest to the highest possible eLoad amount, the Daps app got your back. Enter your mobile number, pay, and wait for confirmation. 
   
 * **Purchase Online**
 
-Shopping online for foods, clothes, furniture, and other essential products is made easier by the Swipe app. Since it is an e-wallet, you can easily check out and pay for your due amount with just a tap. However, you need to link your Swipe account to the store where you’ll purchase the product. This is where the linking of your debit card is essential. There are stores that don’t have the Swipe app on their list of payment methods. In this case, you can still use the fund on the app by linking your card. It only takes a couple of steps to do that and you’re ready to shop securely with confidence.
+Shopping online for foods, clothes, furniture, and other essential products is made easier by the Daps app. Since it is an e-wallet, you can easily check out and pay for your due amount with just a tap. However, you need to link your Daps account to the store where you’ll purchase the product. This is where the linking of your debit card is essential. There are stores that don’t have the Daps app on their list of payment methods. In this case, you can still use the fund on the app by linking your card. It only takes a couple of steps to do that and you’re ready to shop securely with confidence.
 
 * **QR Code**
 
-The Swipe app also generates QR codes if you want a seamless payment on the counter. It is helpful in stores that support payment through e-wallet.
+The Daps app also generates QR codes if you want a seamless payment on the counter. It is helpful in stores that support payment through e-wallet.
   
-* **CASH-IN THE SWIPE ACCOUNT**
+* **CASH-IN THE DAPS ACCOUNT**
 
-You may top up or load your Swipe wallet either through mobile payment facilities available in the application or by going to Swipe Partners Outlets like remittance centers, malls, and convenience stores. The minimum amount you can top up is Php 500.00. Once you have enough money in your wallet, you can start the mobile transactions you desire
+You may top up or load your Daps wallet either through mobile payment facilities available in the application or by going to Daps Partners Outlets like remittance centers, malls, and convenience stores. The minimum amount you can top up is Php 500.00. Once you have enough money in your wallet, you can start the mobile transactions you desire
 
 * **LINKED BANK ACCOUNTS**
 
-For faster transactions, you might need to link your bank account to the Swipe app. Linking your credit and debit card is helpful, especially when you want to transfer money from the Swipe app to your bank. Rest assured that your information is kept secured and confidential by the system. Here are the following bank accounts that you can link.
+For faster transactions, you might need to link your bank account to the Daps app. Linking your credit and debit card is helpful, especially when you want to transfer money from the Daps app to your bank. Rest assured that your information is kept secured and confidential by the system. Here are the following bank accounts that you can link.
 
 * American Express Virtual Pay
 * Paypal
@@ -189,11 +189,11 @@ For faster transactions, you might need to link your bank account to the Swipe a
 * Any existing debit card you have
 
   
-**Tips in using the Swipe app**
+**Tips in using the Daps app**
 
-* Never share nor store your Swipe account and password to anyone or anywhere.
+* Never share nor store your Daps account and password to anyone or anywhere.
 * If you are new to mobile wallets, start by cashing-in small amounts first until you get familiarized with the app and services the app offers.
-* Always have a stable and secured internet connection when doing transactions on the Swipe app.
-* Do not get caught by phishing emails that contain malicious links that, when opened, the scammer can access your Swipe account.
+* Always have a stable and secured internet connection when doing transactions on the Daps app.
+* Do not get caught by phishing emails that contain malicious links that, when opened, the scammer can access your Daps account.
 
 """;
